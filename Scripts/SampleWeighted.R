48e9cc2d8c8c4089c4b894620ec8eb21570f36bb
Narjes Bessghaier <60219876+Narjes-b@users.noreply.github.com>
2023-12-14 21:32:55 -0500

    Add files via upload

---
+++ Scripts/SampleWeighted.R
@@ -0,0 +1,38 @@
+library(dplyr)
+
+
+desired_sample_size <- 379
+
+
+data<- read.csv("IAC-CodeReviews.csv")
+
+
+
+# Sample one developer from each unique company
+unique_samples <- data %>%
+  group_by(Phase) %>%
+  sample_n(size = 1) %>%
+  ungroup()
+
+# Calculate the remaining slots needed
+remaining_slots <- desired_sample_size - nrow(unique_samples)
+
+# Calculate the adjusted sampling probabilities
+adjusted_probabilities <- table(data$Phase) / sum(table(data$Phase))
+adjusted_probabilities
+# Sample the remaining slots from developers, ensuring each company is represented
+additional_samples <- data %>%
+  anti_join(unique_samples, by = "ChangeID") %>%  # Exclude already sampled developers
+  sample_n(size = remaining_slots, replace = TRUE, prob = adjusted_probabilities) %>%
+  ungroup()
+
+# Combine the unique samples with additional samples
+#mysample <- bind_rows(unique_samples, additional_samples)
+#mysample
+
+mysample <- bind_rows(unique_samples, additional_samples) %>%
+  distinct(ChangeID, .keep_all = TRUE)
+mysample
+
+write.csv(mysample,"WeightedSampleR_.csv", row.names = FALSE)
+
